cd3eb3fd34952c612bcc07c1fa1939f1cfbd1093
Tim Guske <108478494+TimGuske@users.noreply.github.com>
2023-01-11 12:51:27 +0100

    delete

--- delete.js
+++
@@ -1,7 +0,0 @@
-export function deleteAddress(address, id) {
-    const parsedId = parseInt(id, 10);
-    const filteredAdress = address.filter(
-        (address) => address.id !== parsedId,
-    );
-    return filteredAdress;
-}
