8c88721c8f26ba88d2b58ef8bebc61ea6f486493
Tim Guske <108478494+TimGuske@users.noreply.github.com>
2024-01-24 13:37:33 +0100

    final push

--- work-countdown-code/src/App.js
+++ work-countdown-code/src/App.js
@@ -1,20 +1,17 @@
 import './styles/App.css';
 import React, { useState, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
 
 function App() {
 
   const [startzeit, setStartzeit] = useState('7:55');
-  const [pause, setPause] = useState('0:30');
+  const [pause, setPause] = useState('0:45');
   const [endzeit, setEndzeit] = useState(getCurrentTime());
-  const [sollArbeitszeit, setSollArbeitszeit] = useState('7.6');
+  const [sollArbeitszeit, setSollArbeitszeit] = useState('7.60');
   const [ueberstunden, setUeberstunden] = useState(null);
   const [arbeitszeit, setArbeitszeit] = useState(null);
-  const [useRealTime, setUseRealTime] = useState(true);
 
   const berechneDaten = () => {
     console.log('Call: berechne Daten');
@@ -23,45 +20,33 @@ function App() {
       return stunden + minuten / 60;
     }
 
+    const currentTime = getCurrentTime();
+    setEndzeit(currentTime);
+
     const startStunden = uhrzeitZuDezimal(startzeit);
     const endStunden = uhrzeitZuDezimal(endzeit);
     const pauseStunden = uhrzeitZuDezimal(pause);
 
     const arbeitszeitT = (endStunden - startStunden - pauseStunden).toFixed(2);
-    const ueberstundenT = (arbeitszeit - sollArbeitszeit).toFixed(2);
+    const ueberstundenT = (arbeitszeitT - sollArbeitszeit).toFixed(2);
 
     setArbeitszeit(arbeitszeitT);
     setUeberstunden(ueberstundenT);
-    console.log(arbeitszeitT, ueberstundenT);
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Überstunden berechnen und in den State speichern
-  };
 
   useEffect(() => {
-    console.log('Call: UseEffect');
-    setWorkTime();
+
     berechneDaten();
 
-    if (useRealTime) {
-      const intervalId = setInterval(() => {
-        console.log('Call: SetInterval');
+    const intervalId = setInterval(() => {
+      berechneDaten();
+    }, 10000);
 
-        setWorkTime();
-        berechneDaten();
-      }, 10000);
+    return () => clearInterval(intervalId);
 
-      return () => clearInterval(intervalId);
-    }
-    
-  }, [startzeit, pause, endzeit, useRealTime]);
+  }, [startzeit, pause, endzeit]);
 
-  const setWorkTime = () => {
-    const currentTime = getCurrentTime();
-    setEndzeit(currentTime);
-  }
 
   function getCurrentTime() {
     console.log('Call: getCurrentTime');
@@ -74,7 +59,7 @@ function App() {
   return (
     <div>
 
-      <Form onSubmit={handleSubmit}>
+      <Form>
         <Form.Group as={Row} className="mb-4" >
           <Form.Label column sm="2"></Form.Label>
           <Col sm="6"><h1>💻 Arbeitszeit Rechner</h1></Col>
@@ -105,6 +90,7 @@ function App() {
           <Form.Label column sm="2">Soll in h</Form.Label>
           <Col sm="6">
             <Form.Control
+              className='bg-dark text-light'
               type="text"
               size="sm"
               value={sollArbeitszeit}
@@ -118,36 +104,20 @@ function App() {
           <Form.Label column sm="2">Endzeit</Form.Label>
           <Col sm="6">
             <Form.Control
+              className='bg-dark text-light'
               size="sm"
               type="text"
               value={endzeit}
-              onChange={(e) => setEndzeit(e.target.value)}
+              disabled
+              readOnly
             />
           </Col>
-        </Form.Group>
-        <Form.Group as={Row} className="mb-5" >
-          <Form.Label column sm="2"></Form.Label>
-          <Col sm="3">
-            <Button variant='secondary' type='submit'>Berechne Überstunden</Button>
-          </Col>
-          <Col sm="4">
-            <Form.Check
-              className='mt-2'
-              type="switch"
-              id="custom-switch"
-              label="RealTime"
-              checked={useRealTime}
-              onChange={(e) => setUseRealTime(e.target.checked)}
-            />
-          </Col>
-        </Form.Group>
+        </Form.Group>      
         <Form.Group as={Row} className="mb-3" >
           <Form.Label column sm="2"></Form.Label>
-          <Col sm="8">
-            <h3 style={{ color: ueberstunden < 0 ? 'red' : 'green' }}>
-              {ueberstunden} Stunden (Arbeitszeit: {arbeitszeit}h)
-            </h3>
-            {useRealTime ? (<p>RealTime aktiv</p>) : (<p>keine Berechnung aktiv </p>)}
+          <Col sm="8" style={{display:'flex'}}>
+            <p style={{ marginRight: '10px' }}><b>{arbeitszeit}</b> / {sollArbeitszeit} Stunden</p>
+            <p style={{ color: ueberstunden < 0 ? 'red' : 'green' }}>({ueberstunden} Überstunden)</p>
           </Col>
         </Form.Group>
       </Form>
